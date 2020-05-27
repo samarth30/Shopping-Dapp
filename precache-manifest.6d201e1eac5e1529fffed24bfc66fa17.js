d39078f57a3d429e20ce36090af6235eab08d91c
samarth <samarth.gugnani30@gmail.com>
2020-05-28 05:17:33 +0530

    Updates

---
+++ precache-manifest.6d201e1eac5e1529fffed24bfc66fa17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "405de2fce3f23c05ded7",
+    "url": "/Shopping-Dapp/static/css/main.f209dcd0.chunk.css"
+  },
+  {
+    "revision": "405de2fce3f23c05ded7",
+    "url": "/Shopping-Dapp/static/js/main.405de2fc.chunk.js"
+  },
+  {
+    "revision": "f2dfaa1c450278877890",
+    "url": "/Shopping-Dapp/static/css/1.cf9b883e.chunk.css"
+  },
+  {
+    "revision": "f2dfaa1c450278877890",
+    "url": "/Shopping-Dapp/static/js/1.f2dfaa1c.chunk.js"
+  },
+  {
+    "revision": "d19d4d930fa45e02a1b9",
+    "url": "/Shopping-Dapp/static/js/runtime~main.d19d4d93.js"
+  },
+  {
+    "revision": "acea7757d6bf94743509f43a3ee9b2b4",
+    "url": "/Shopping-Dapp/index.html"
+  }
+];
